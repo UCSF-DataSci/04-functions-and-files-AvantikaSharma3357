7c9f975b06bc1dd70cf6a4ce21f42bbc207c5c24
AvantikaSharma3357 <181284090+AvantikaSharma3357@users.noreply.github.com>
2024-10-09 07:27:16 +0000

    Assignment 4 submission

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
+import argparse
 """
 Fibonacci Sequence
 
@@ -14,10 +15,35 @@ Create a program that generates Fibonacci numbers less than a limit and writes t
 Task: Generate the Fibonacci numbers less than 100 and write them to `fibonacci_100.txt`
 """
 
-def some_function(an_argument):
+def fibonnaci_function(n): 
     # Do something
+    fibonnaci_list = []
+    if n >= 0: 
+        fibonnaci_list.append(0)
+        
+    if n >= 1:
+        fibonnaci_list.append(1)
+    
+    if n > 1:
+        i = 2 
+        while i < n: 
+            fibonnaci_list.append(fibonnaci_list[i-2] + fibonnaci_list[i-1])
+            i += 1
 
-    return something
+    return fibonnaci_list
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename")
+parser.add_argument("-c", "--count", type=int, default=1)
+args = parser.parse_args()
+
+##python fibonnaci.py fibonacci_100.txt -c 100 
 
 if __name__ == "__main__":
     # Do stuff here
+    fibonnaci_list = fibonnaci_function(args.count)
+    with open(args.filename,'w') as file:
+       for number in fibonnaci_list: 
+       	file.write(str(number)+ "\n")
--- largest_prime.py
+++ largest_prime.py
@@ -12,3 +12,36 @@ Task: Find the largest prime Fibonacci number less that 50000
 """
 
 # You're on your own for this one. Good luck!
+
+from fibonnaci import fibonnaci_function
+import argparse
+import sympy
+
+def is_prime(num): 
+    for i in range(2, int(round(num*0.5)) + 1): 
+        if num % i == 0:
+            return False
+    return True
+
+def find_largest_prime(fib_list): 
+    fib_list = fib_list[::-1]
+    for num in fib_list:
+        ##a = is_prime(num)
+        a = sympy.isprime(num)
+        if a: 
+            return num
+
+
+parser_1 = argparse.ArgumentParser()
+parser_1.add_argument("filename", type=str, help="The filename to process")  # Positional argument
+parser_1.add_argument("-c", "--count", type=int, default=1)
+args = parser_1.parse_args()
+
+
+##python largest_prime.py myfile.txt -c 50000
+
+if __name__ == "__main__":
+    # Do stuff here
+    fibonnaci_list = fibonnaci_function(args.count)
+    largest_prime = find_largest_prime(fibonnaci_list)
+    print(largest_prime) 
